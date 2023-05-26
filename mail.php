da4282bfecd96f35c6d3ce9f44cc5527ecef0665
Agricio_neto <nerovigiann@hotmail.com>
2023-05-25 20:42:11 -0400

    first commit

---
+++ mail.php
@@ -0,0 +1,28 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$type = $_POST['project'];
+$message = $_POST['message'];
+$recipient = "santosgeneralservices12@gmail.com";
+$subject = "project";
+$mailheader = "From: $email \r\n";
+
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+
+$mail_status = mail($recipient, $subject, $formcontent, $mailheader);
+
+//if($mail_status == true){
+//    echo "<script>alert('Thank you for the message. We will contact you shortly.');</script>";
+//    header('Location:https://santosgeneralservice.com/index.html');
+//}else{
+//    echo "<script>alert('Sorry! Please try again.');</script>";
+//    header('Location:https://santosgeneralservice.com/pages/request.html');/
+//}
+
+if ($mail_status) {    
+        echo "<script>alert('Thank you for the message. We will contact you shortly.');</script>
+        <script type='text/javascript'>window.location.href='https://santosgeneralservice.com/index.html'</script>"; 
+    } else {
+         echo "<script>alert('Sorry! Please try again.');</script>
+        <script type='text/javascript'>window.location.href='https://santosgeneralservice.com/pages/request.html'</script>"; }
+  ?>
