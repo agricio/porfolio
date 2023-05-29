4551c2b0041a90724d9fd5bd95fae82499f9119e
Agricio_neto <nerovigiann@hotmail.com>
2023-05-29 10:44:31 -0400

    saddfsdf

--- api/mail_send.php
+++ api/mail_send.php
@@ -17,7 +17,7 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.live.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'nerovigiann@gmail.com';                     //SMTP username
+    $mail->Username   = 'nerovigiann@hotmail.com';                     //SMTP username
     $mail->Password   = 'Snowbreese00';                               //SMTP password
     //$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
