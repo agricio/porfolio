818c7fbcbd4c7b5d88853880e7f6c4a0de23a17b
Agricio_neto <nerovigiann@hotmail.com>
2023-05-29 15:40:52 -0400

    set PHP

--- api/mail_send.php
+++ api/mail_send.php
@@ -13,8 +13,8 @@ $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
+    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                           //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'nerovigiann@gmail.com';                     //SMTP username
@@ -23,10 +23,11 @@ try {
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
+    $mail->client_email = ($_POST['email']);
     $mail->setFrom('nerovigiann@gmail.com', 'Mailer');
-    $mail->addAddress('nerovigiann@hotmail.com', 'Joe User');     //Add a recipient
+    $mail->addAddress('nerovigiann@hotmail.com', $_POST['name'] );     //Add a recipient
     //$mail->addAddress('ellen@example.com');               //Name is optional
-    $mail->addReplyTo('nerovigiann@gmail.com', 'Information');
+    $mail->addReplyTo('client_email', 'Information');
    // $mail->addCC('cc@example.com');
    // $mail->addBCC('bcc@example.com');
 
@@ -36,12 +37,16 @@ try {
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->Subject = ($_POST['project']);
+    $mail->Body    = ($_POST['message']);
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
+ //   echo 'Message has been sent';
+     echo "<script>alert('Thank you for the message. We will contact you shortly.');</script>
+        <script type='text/javascript'>window.location.href='https://porfolio-khaki.vercel.app/#home'</script>";
+    } catch (Exception $e) {
+        echo "<script>alert('Sorry! Please try again.');</script>
+        <script type='text/javascript'>window.location.href='https://porfolio-khaki.vercel.app/#contact'</script>"; 
+    }
+
