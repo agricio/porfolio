fc6babd52dcff303b65cdea4c1535acae2898d72
Agricio_neto <nerovigiann@hotmail.com>
2023-06-21 15:32:17 -0400

    add ambient variable

--- api/mail_send.php
+++ api/mail_send.php
@@ -5,6 +5,15 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
+//ambient variables
+$parsed = parse_ini_file('./.env', true);
+
+$_ENV[ENVIRONMENT] = $parsed['ENVIRONMENT'];
+
+foreach ($parsed[$parsed['ENVIRONMENT']] as $key => $value) {
+    $_ENV[$key] = $value;
+}
+
 //Load Composer's autoloader
 require 'vendor/autoload.php';
 
@@ -17,17 +26,17 @@ try {
     $mail->isSMTP();                                           //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'nerovigiann@gmail.com';                     //SMTP username
-    $mail->Password   = 'egdtlgxgpxfgrpqf';                               //SMTP password
+    $mail->Username   = $_ENV['EMAIL'];                     //SMTP username
+    $mail->Password   = $_ENV['PASSWORD'];                              //SMTP password
     //$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->client_email = ('nerovigiann@gmail.com');
-    $mail->setFrom('nerovigiann@gmail.com', ($_POST['name']));
-    $mail->addAddress('nerovigiann@hotmail.com', 'Agricio');     //Add a recipient
+    $mail->client_email = $_ENV['EMAIL'];
+    $mail->setFrom($_ENV['EMAIL'], ($_POST['name']));
+    $mail->addAddress($_ENV['EMAIL'], 'Agricio');     //Add a recipient
     //$mail->addAddress('ellen@example.com');               //Name is optional
-    $mail->addReplyTo('nerovigiann@hotmail.com', 'Information');
+    $mail->addReplyTo($_ENV['EMAIL'], 'Information');
    // $mail->addCC('cc@example.com');
    // $mail->addBCC('bcc@example.com');
 
